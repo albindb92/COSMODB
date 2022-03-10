4b93d18e62d1ed843da23ccf3b0447f0fe032cea
albindb92 <albindb92@users.noreply.github.com>
2022-03-11 01:35:19 +0530

    CosmoDB with Java Spring boot

---
+++ src/test/java/com/springboot/cosmosdb/CosmosDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.cosmosdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
